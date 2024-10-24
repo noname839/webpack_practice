32b2e411f31d94f4c6c0153fa21b8527060a4f34
name <173113258+noname839@users.noreply.github.com>
2024-10-24 15:34:55 +0900

    Install css-loader & style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,22 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        hashFunction: 'xxhash64'
-    }
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+        ],
+    },
 }
